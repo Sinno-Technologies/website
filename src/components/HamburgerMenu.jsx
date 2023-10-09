991f5ba3dadbd238f15dac3f2c9ef27a630c6e5f
Jamil Sinno <jamil.sinno@gmail.com>
2023-10-08 21:02:09 -0300

    fixed hamburger menu

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-
+import { useStore } from '@nanostores/react';
+import { isNavOpen } from '../stores/navStore';
 const HamburgerMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    console.log('button')
-    console.log(isOpen);
-    setIsOpen(!isOpen);
-  };
-
+  const $isNavOpen = useStore(isNavOpen); 
   return (
-    <div className="hamburger-menu">
-      <button className={`hamburger-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </button>
-      {isOpen && <section>
-        <nav class="navLinks">
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </section>}
-    </div>
+    <>
+      <div className="hamburger-menu" onClick={() => isNavOpen.set(!$isNavOpen)}>
+        {$isNavOpen === false && <><div className="burger"></div>
+        <div className="burger"></div>
+        <div className="burger"></div></>}
+        {$isNavOpen === true && <><div className="burger burger1"></div>
+        <div className="burger burger2"></div>
+        <div className="burger burger3"></div></>}
+      </div>
+      <style jsx="true">{`
+        .hamburger-menu {
+          width: 2rem;
+          height: 2rem;
+          display: flex;
+          flex-flow: column nowrap;
+          justify-content: space-around;
+          cursor: pointer;
+        } 
+
+        .burger{
+          width: 2rem;
+          height: 0.25rem;
+          border-radius: 0.5rem;
+          background-color: var(--yellow);
+          transform-origin: 1px;
+          transition: all 0.3s ease-in-out;
+        }
+
+        .burger1{
+          transform: ${$isNavOpen ? 'rotate(45deg)' : 'rotate(0)'};
+        }
+        .burger2{
+          opacity: ${$isNavOpen ? '0' : '1'};
+          transform: ${$isNavOpen ? 'translateX(100%)' : 'translateX(0)'};
+        }
+        .burger3{
+          transform: ${$isNavOpen ? 'rotate(-45deg)' : 'rotate(0)'};
+        }
+      `}</style>
+    </>
   );
 };
 
 export default HamburgerMenu;
+
