f759bd66b5e9c64bbd73c4320d1fe96d0f08819c
maged1989 <141666171+maged1989@users.noreply.github.com>
2023-10-30 19:22:33 -0300

    added templates from personal website

---
+++ src/data/cardData.js
@@ -0,0 +1,32 @@
+const cardData = [
+    {
+        title: "Parkland Property Management",
+        body: "Local Business - Static website designed to customer specification using Astro.js and Vanilla CSS.",
+        img: "/images/ParklandPM.png",
+        alt: "Parkland Property Management Home Page",
+        href: "https://www.parklandpm.ca/",
+    },
+    {
+        title: "UNUM Health",
+        body: "Start up - Full Stack Application built with React, PHP, and PostgreSQL in Docker environment. Contract complete.",
+        img: "/images/unumHealth.png",
+        alt: "Unum Health Login Page",
+        href: "#",
+    },
+    {
+        title: "No Game Left Behind",
+        body: "Designed in HTML, CSS, JavaScript, EJS, Node.js, Express, Passport.js, Tailwind CSS, Daisy UI, MongoDB",
+        img: "/images/Login.png",
+        alt: "Front page of No Game Left Behind",
+        href: "#",
+    },
+    {
+        title: "R.A.M.P. Atlantic Ltd.",
+        body: "Business Website - Civil Engineering Firm. Designed in HTML, CSS & JS",
+        img: "/images/rampatl.png",
+        alt: "RAMP Atlantic Home Page",
+        href: "https://rampatlantic.netlify.app/index.html",
+    },
+]
+
+export default cardData;
---
+++ src/data/googleReviews.js
@@ -0,0 +1,19 @@
+const reviews = [
+    {
+        author: "Maged Aly",
+        stars: "5 stars",
+        body: "Thank you Sinno Technologies for creating my website. I was looking for a minimalistic design, and they delivered exactly what I was looking for in a record time. Jamil is very personable, and will work with you until you are satisfied. Definitely recommended."
+    },
+    {
+        author: "Cristian Matos Richards",
+        stars: "5 stars",
+        body: "I've teamed up with Jamil on multiple occasions, and I've gotta say, his knack for understanding exactly what a project needs is just the start to why its great to work with them.",
+    },
+    {
+        author: "Barbara Odozi",
+        stars: "5 stars",
+        body: "I was very impressed with the quality of work from Sinno Technologies Ltd. They were able to take my vision and bring it to life in a way that I never could have imagined. I would definitely use them again for future projects.",
+    },
+]
+
+export default reviews;
