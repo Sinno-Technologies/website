72f9fdc74e6f6ed645183cb355ab7fd3dd4ed887
Jamil Sinno <jamil.sinno@gmail.com>
2023-11-11 18:34:33 -0400

    started rss feed

---
+++ src/feed.xml.ts
@@ -0,0 +1,16 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext){
+    return rss({
+        title: "Sinno Technologies Limited - RSS Feed",
+        description: "Sinno Technologies Limited is a Canadian based software development company.",
+        site: context.site?.toString() || "https://www.sinno.ca",
+        items:[
+            {
+                title: "Sinno Technologies Limited",
+                link: "https://www.sinno.ca",
+                pubDate: new Date()
+            }
+        ]})
+    }
