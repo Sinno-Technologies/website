79d7645afa838a120ef336b418889d8d156fdab0
Jamil <jamil.sinno@gmail.com>
2024-07-03 17:04:54 -0300

    added unum link

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,40 +1,40 @@
 const cardData = [
-    {
-        title: "Parkland Property Management",
-        body: "Local Business - Static website designed to customer specification using Astro.js and Vanilla CSS.",
-        img: "/src/assets/ParklandPM.png",
-        alt: "Parkland Property Management Home Page",
-        href: "https://www.parklandpm.ca/",
-        target:"_blank",
-        rel: "noreferrer noopener nofollow" 
-    },
-    {
-        title: "UNUM Health",
-        body: "Start up - Full Stack Application built with React, PHP, and PostgreSQL in Docker environment. Contract complete.",
-        img: "/src/assets/unumHealth.png",
-        alt: "Unum Health Login Page",
-        href: "#projects",
-        target:"",
-        rel: "" 
-    },
-    {
-        title: "No Game Left Behind",
-        body: "Designed in HTML, CSS, JavaScript, EJS, Node.js, Express, Passport.js, Tailwind CSS, Daisy UI, MongoDB",
-        img: "/src/assets/Login.png",
-        alt: "Front page of No Game Left Behind",
-        href: "#projects",
-        target:"",
-        rel: "" 
-    },
-    {
-        title: "R.A.M.P. Atlantic Ltd.",
-        body: "Business Website - Civil Engineering Firm. Designed in HTML, CSS & JS",
-        img: "/src/assets/rampatl.png",
-        alt: "RAMP Atlantic Home Page",
-        href: "https://rampatlantic.netlify.app/index.html",
-        target:"_blank",
-        rel: "noreferrer noopener nofollow" 
-    },
+  {
+    title: "Parkland Property Management",
+    body: "Local Business - Static website designed to customer specification using Astro.js and Vanilla CSS.",
+    img: "/src/assets/ParklandPM.png",
+    alt: "Parkland Property Management Home Page",
+    href: "https://www.parklandpm.ca/",
+    target: "_blank",
+    rel: "noreferrer noopener nofollow",
+  },
+  {
+    title: "UNUM Health",
+    body: "Start up - Full Stack Application built with React, PHP, and PostgreSQL in Docker environment. Contract complete.",
+    img: "/src/assets/unumHealth.png",
+    alt: "Unum Health Login Page",
+    href: "https://www.unumhealth.com",
+    target: "",
+    rel: "",
+  },
+  {
+    title: "No Game Left Behind",
+    body: "Designed in HTML, CSS, JavaScript, EJS, Node.js, Express, Passport.js, Tailwind CSS, Daisy UI, MongoDB",
+    img: "/src/assets/Login.png",
+    alt: "Front page of No Game Left Behind",
+    href: "#projects",
+    target: "",
+    rel: "",
+  },
+  {
+    title: "R.A.M.P. Atlantic Ltd.",
+    body: "Business Website - Civil Engineering Firm. Designed in HTML, CSS & JS",
+    img: "/src/assets/rampatl.png",
+    alt: "RAMP Atlantic Home Page",
+    href: "https://rampatlantic.netlify.app/index.html",
+    target: "_blank",
+    rel: "noreferrer noopener nofollow",
+  },
 ]
 
-export default cardData;
+export default cardData
