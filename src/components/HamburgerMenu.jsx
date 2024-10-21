d269d033f87bdaac2c594529b1fbcbb038cc9f78
Jamil <jamil.sinno@gmail.com>
2024-10-21 12:19:07 -0300

    refactor: added light header icons

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,5 +1,10 @@
 import { useStore } from "@nanostores/react"
 import { isNavOpen } from "../stores/navStore"
+import { Image } from "astro:assets"
+// import { Burger } from "../assets/icons/burger_icon_white.svg"
+// import { Close } from "../assets/icons/close_icon_white.svg"
+// import { ReactComponent as BurgerIcon } from "../assets/icons/burger_icon_white.svg"
+// import { ReactComponent as CloseIcon } from "../assets/icons/close_icon_white.svg"
 const HamburgerMenu = () => {
   const $isNavOpen = useStore(isNavOpen)
   return (
@@ -11,16 +16,18 @@ const HamburgerMenu = () => {
       {!$isNavOpen && (
         <img
           className="imgBurger"
-          src="/icons/burger_icon.png"
+          src="/icons/burger_icon_white.svg"
           alt="Open menu"
+          title="Open menu"
           aria-label="Open menu"
         />
       )}
       {$isNavOpen && (
         <img
           className="imgBurger"
-          src="/icons/close_icon.png"
+          src="/icons/close_icon_white.svg"
           alt="Close menu"
+          title="Close menu"
           aria-label="Close menu"
         />
       )}
@@ -39,7 +46,7 @@ const HamburgerMenu = () => {
           justify-content: space-around;
           align-items: center;
           cursor: pointer;
-          background-color: var(--black);
+          background-color: var(--white);
           border: none;
         }
 
