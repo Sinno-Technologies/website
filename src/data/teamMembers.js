751b15ab04d4faa83c11a892287cca8cca68a4b9
Jamil <jamil.sinno@gmail.com>
2025-01-13 23:46:42 -0400

    changed hero section and fixed resume url

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -8,7 +8,7 @@ export const teamMembers = [
     bio: "Jamil is a Full Stack Software Developer with 10 years of experience, specializing in Web Accessibility. He builds applications and ensures they meet the Web Content Accessibility Guidelines (WCAG).",
     image: Jamil,
     linkedInUrl: "https://www.linkedin.com/in/jamilsinno",
-    resume: "/public/resume.pdf",
+    resume: "../../public/resume.pdf",
   },
   {
     name: "Jalal Sinno",
@@ -16,6 +16,6 @@ export const teamMembers = [
     bio: "Jalal is an expert in Cybersecurity and Testing. With 6 years of experience in the field, he ensures applications are secure against vulnerabilities and exploits.",
     image: Jalal,
     linkedInUrl: "https://www.linkedin.com/in/jalal-sinno",
-    resume: "/public/jalal_resume.pdf",
+    resume: "../../public/jalal_resume.pdf",
   },
 ];
