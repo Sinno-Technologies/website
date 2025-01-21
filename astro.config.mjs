acd2dd0ddd66c585a9e48b3763b9905cda1279eb
Jamil <jamil.sinno@gmail.com>
2025-01-21 11:40:47 -0400

    Improved site performance, updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sinno.ca',
   integrations: [react(), sitemap()],
-  experimental: {
-    devOverlay: true
-  }
 });
